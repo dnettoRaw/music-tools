f5449f62724a1eeff50cbd6cde9af28cbc5d5b06
dnettoRaw <me@dnetto.dev>
2024-05-12 20:47:14 +0200

    fix rename

--- white-space/fix-names-to-ascii.sh
+++ white-space/fix-names-to-ascii.sh
@@ -1,46 +1,83 @@
-#!/bin/bah
+#!/bin/bash
 
-#dnetto v0.0.2
+# dnetto v0.2.1 - Improved performance with logging
 
-# this script is a wrapper for mp3gain
-# it normalize all mp3 files in the current directory
+# Normalize all MP3 files in the current directory using mp3gain (wrapper script)
 
+debug=0
 
-declare debug=0
-
-declare -a acc
-declare -a noa
-acc=('$' 'Ã¨' 'Ãª' 'Ã©' 'À' 'Á' 'Â' 'Ã' 'Ä' 'Å' 'Æ' 'Ç' 'È' 'É' 'Ê' 'Ë' 'Ì' 'Í' 'Î' 'Ï' 'Ð' 'Ñ' 'Ò' 'Ó' 'Ô' 'Õ' 'Ö' 'Ø' 'Ù' 'Ú' 'Û' 'Ü' 'Ý' 'ß' 'à' 'á' 'â' 'ã' 'ä' 'å' 'æ' 'ç' 'è' 'é' 'ê' 'ë' 'ì' 'í' 'î' 'ï' 'ñ' 'ò' 'ó' 'ô' 'õ' 'ö' 'ø' 'ù' 'ú' 'û' 'ü' 'ý' 'ÿ' 'Ā' 'ā' 'Ă' 'ă' 'Ą' 'ą' 'Ć' 'ć' 'Ĉ' 'ĉ' 'Ċ' 'ċ' 'Č' 'č' 'Ď' 'ď' 'Đ' 'đ' 'Ē' 'ē' 'Ĕ' 'ĕ' 'Ė' 'ė' 'Ę' 'ę' 'Ě' 'ě' 'Ĝ' 'ĝ' 'Ğ' 'ğ' 'Ġ' 'ġ' 'Ģ' 'ģ' 'Ĥ' 'ĥ' 'Ħ' 'ħ' 'Ĩ' 'ĩ' 'Ī' 'ī' 'Ĭ' 'ĭ' 'Į' 'į' 'İ' 'ı' 'Ĳ' 'ĳ' 'Ĵ' 'ĵ' 'Ķ' 'ķ' 'Ĺ' 'ĺ' 'Ļ' 'ļ' 'Ľ' 'ľ' 'Ŀ' 'ŀ' 'Ł' 'ł' 'Ń' 'ń' 'Ņ' 'ņ' 'Ň' 'ň' 'ŉ' 'Ō' 'ō' 'Ŏ' 'ŏ' 'Ő' 'ő' 'Œ' 'œ' 'Ŕ' 'ŕ' 'Ŗ' 'ŗ' 'Ř' 'ř' 'Ś' 'ś' 'Ŝ' 'ŝ' 'Ş' 'ş' 'Š' 'š' 'Ţ' 'ţ' 'Ť' 'ť' 'Ŧ' 'ŧ' 'Ũ' 'ũ' 'Ū' 'ū' 'Ŭ' 'ŭ' 'Ů' 'ů' 'Ű' 'ű' 'Ų' 'ų' 'Ŵ' 'ŵ' 'Ŷ' 'ŷ' 'Ÿ' 'Ź' 'ź' 'Ż' 'ż' 'Ž' 'ž' 'ſ' 'ƒ' 'Ơ' 'ơ' 'Ư' 'ư' 'Ǎ' 'ǎ' 'Ǐ' 'ǐ' 'Ǒ' 'ǒ' 'Ǔ' 'ǔ' 'Ǖ' 'ǖ' 'Ǘ' 'ǘ' 'Ǚ' 'ǚ' 'Ǜ' 'ǜ' 'Ǻ' 'ǻ' 'Ǽ' 'ǽ' 'Ǿ' 'ǿ' '&' );
-
-noa=('S' 'e' 'e' 'e' 'A' 'A' 'A' 'A' 'A' 'A' 'AE' 'C' 'E' 'E' 'E' 'E' 'I' 'I' 'I' 'I' 'D' 'N' 'O' 'O' 'O' 'O' 'O' 'O' 'U' 'U' 'U' 'U' 'Y' 's' 'a' 'a' 'a' 'a' 'a' 'a' 'ae' 'c' 'e' 'e' 'e' 'e' 'i' 'i' 'i' 'i' 'n' 'o' 'o' 'o' 'o' 'o' 'o' 'u' 'u' 'u' 'u' 'y' 'y' 'A' 'a' 'A' 'a' 'A' 'a' 'C' 'c' 'C' 'c' 'C' 'c' 'C' 'c' 'D' 'd' 'D' 'd' 'E' 'e' 'E' 'e' 'E' 'e' 'E' 'e' 'E' 'e' 'G' 'g' 'G' 'g' 'G' 'g' 'G' 'g' 'H' 'h' 'H' 'h' 'I' 'i' 'I' 'i' 'I' 'i' 'I' 'i' 'I' 'i' 'IJ' 'ij' 'J' 'j' 'K' 'k' 'L' 'l' 'L' 'l' 'L' 'l' 'L' 'l' 'l' 'l' 'N' 'n' 'N' 'n' 'N' 'n' 'n' 'O' 'o' 'O' 'o' 'O' 'o' 'OE' 'oe' 'R' 'r' 'R' 'r' 'R' 'r' 'S' 's' 'S' 's' 'S' 's' 'S' 's' 'T' 't' 'T' 't' 'T' 't' 'U' 'u' 'U' 'u' 'U' 'u' 'U' 'u' 'U' 'u' 'U' 'u' 'W' 'w' 'Y' 'y' 'Y' 'Z' 'z' 'Z' 'z' 'Z' 'z' 's' 'f' 'O' 'o' 'U' 'u' 'A' 'a' 'I' 'i' 'O' 'o' 'U' 'u' 'U' 'u' 'U' 'u' 'U' 'u' 'U' 'u' 'A' 'a' 'AE' 'ae' 'O' 'o' 'e');
+declare -a replace_map=(
+  '$'='S' 'Ã¨'='e' 'Ãª'='e' 'Ã©'='e' 'À'='A' 'Á'='A' 'Â'='A' 'Ã'='A' 'Ä'='A' 'Å'='A' 'Æ'='AE' 'Ç'='C'
+  'È'='E' 'É'='E' 'Ê'='E' 'Ë'='E' 'Ì'='I' 'Í'='I' 'Î'='I' 'Ï'='I' 'Ð'='D' 'Ñ'='N' 'Ò'='O' 'Ó'='O' 'Ô'='O'
+  'Õ'='O' 'Ö'='O' 'Ø'='O' 'Ù'='U' 'Ú'='U' 'Û'='U' 'Ü'='U' 'Ý'='Y' 'ß'='s' 'à'='a' 'á'='a' 'â'='a' 'ã'='a'
+  'ä'='a' 'å'='a' 'æ'='ae' 'ç'='c' 'è'='e' 'é'='e' 'ê'='e' 'ë'='e' 'ì'='i' 'í'='i' 'î'='i' 'ï'='i' 'ñ'='n'
+  'ò'='o' 'ó'='o' 'ô'='o' 'õ'='o' 'ö'='o' 'ø'='o' 'ù'='u' 'ú'='u' 'û'='u' 'ü'='u' 'ý'='y' 'ÿ'='y' 'Ā'='A'
+  'ā'='a' 'Ă'='A' 'ă'='a' 'Ą'='A' 'ą'='a' 'Ć'='C' 'ć'='c' 'Ĉ'='C' 'ĉ'='c' 'Ċ'='C' 'ċ'='c' 'Č'='C' 'č'='c'
+  'Ď'='D' 'ď'='d' 'Đ'='D' 'đ'='d' 'Ē'='E' 'ē'='e' 'Ĕ'='E' 'ĕ'='e' 'Ė'='E' 'ė'='e' 'Ę'='E' 'ę'='e' 'Ě'='E'
+  'ě'='e' 'Ĝ'='G' 'ĝ'='g' 'Ğ'='G' 'ğ'='g' 'Ġ'='G' 'ġ'='g' 'Ģ'='G' 'ģ'='g' 'Ĥ'='H' 'ĥ'='h' 'Ħ'='H' 'ħ'='h'
+  'Ĩ'='I' 'ĩ'='i' 'Ī'='I' 'ī'='i' 'Ĭ'='I' 'ĭ'='i' 'Į'='I' 'į'='i' 'İ'='I' 'ı'='i' 'Ĳ'='IJ' 'ĳ'='ij' 'Ĵ'='J'
+  'ĵ'='j' 'Ķ'='K' 'ķ'='k' 'Ĺ'='L' 'ĺ'='l' 'Ļ'='L' 'ļ'='l' 'Ľ'='L' 'ľ'='l' 'Ŀ'='L' 'ŀ'='l' 'Ł'='L' 'ł'='l'
+  'Ń'='N' 'ń'='n' 'Ņ'='N' 'ņ'='n' 'Ň'='N' 'ň'='n' 'ŉ'='n' 'Ō'='O' 'ō'='o' 'Ŏ'='O' 'ŏ'='o' 'Ő'='O' 'ő'='o'
+  'Œ'='OE' 'œ'='oe' 'Ŕ'='R' 'ŕ'='r' 'Ŗ'='R' 'ŗ'='r' 'Ř'='R' 'ř'='r' 'Ś'='S' 'ś'='s' 'Ŝ'='S' 'ŝ'='s' 'Ş'='S'
+  'ş'='s' 'Š'='S' 'š'='s' 'Ţ'='T' 'ţ'='t' 'Ť'='T' 'ť'='t' 'Ŧ'='T' 'ŧ'='t' 'Ũ'='U' 'ũ'='u' 'Ū'='U' 'ū'='u'
+  'Ŭ'='U' 'ŭ'='u' 'Ů'='U' 'ů'='u' 'Ű'='U' 'ű'='u' 'Ų'='U' 'ų'='u' 'Ŵ'='W' 'ŵ'='w' 'Ŷ'='Y' 'ŷ'='y' 'Ÿ'='Y'
+  'Ź'='Z' 'ź'='z' 'Ż'='Z' 'ż'='z' 'Ž'='Z' 'ž'='z' 'ſ'='s' 'ƒ'='f' 'Ơ'='O' 'ơ'='o' 'Ư'='U' 'ư'='u' 'Ǎ'='A'
+  'ǎ'='a' 'Ǐ'='I' 'ǐ'='i' 'Ǒ'='O' 'ǒ'='o' 'Ǔ'='U' 'ǔ'='u' 'Ǖ'='U' 'ǖ'='u' 'Ǘ'='U' 'ǘ'='u' 'Ǚ'='U' 'ǚ'='u'
+  'Ǜ'='U' 'ǜ'='u' 'Ǻ'='A' 'ǻ'='a' 'Ǽ'='AE' 'ǽ'='ae' 'Ǿ'='O' 'ǿ'='o' '&'='e' ' '='-' 
+)
 
 function namefix(){
-    declare temp 
-    temp=$1
-    for (( i=0; i<${#acc[@]}; i++ )); do
-        temp=`echo $temp | sed "s/${acc[$i]}/${noa[$i]}/g"`
+    local filename="$1"
+    local fixedname=$(echo "$filename" | tr '[:upper:]' '[:lower:]')  # Converte para minúsculas usando tr
+    fixedname="${fixedname//[^a-zA-Z0-9.]/-}" # Substitui caracteres especiais por hífen
+    # retira os numero do nome do arquivo 
+    fixedname=$(echo "$fixedname" | tr -d '0-9')
 
+    for key in "${!replace_map[@]}"; do
+      fixedname="${fixedname//${key}/${replace_map[$key]}}"
     done
-    echo $temp | tr '[:upper:]' '[:lower:]' | tr -s ' ' | tr -s '_' | tr -s '-' | tr -d ',' | tr -d '(' | tr -d ')'| tr -d '’' | tr -d "'"  | tr -d '' | rev | cut -c2- | rev 
+    
+    echo "${fixedname}3" | tr -s '-'  # Remove hífens extras
 }
 
-function whitespace(){
-    declare temp
-    temp=$1
-    temp=`echo $temp | tr -s ' ' | tr ' ' '-'`
-    echo $temp
+function printFormated(){
+    local original="$1"
+    local newname="$2"
+    local current="$3"
+    local total="$4"
+    local percent=$((current * 100 / total))
+    printf "\033[${printedLines}A"  # Move cursor up
+    printf "[%-50s] %3d%%\n" "$(printf '%*s' $((percent / 2)) '' | tr ' ' '=')" "$percent"
+    printf "[\033[38;5;124m%s\033[0m]\033[K\n" "$original"
+    printf "[\033[38;5;46m%s\033[0m]\033[K\n" "$newname"
+    printedLines=3
 }
 
 function rename(){
+    local original="$1"
+    local newname="$2"
     if [[ debug -eq 1 ]]; then
-        printf "rename \033[38;5;124m$1 \033[38;5;46m$2\033[0m\n"
+        echo "Debug: Renaming $original to $newname"
+        ((printedLines++))  # Increment printed lines
     else
-        mv "$1" "$2"
+        mv -n "$original" "$newname"
     fi
-
 }
 
-for f in "$@" ; do 
-    # echo "actual $f"
-	rename "$f" "$(namefix "$f")"
+# Main processing
+echo "Starting renaming process..."
+total_files=$(ls *.mp3 | wc -l)
+current_file=0
+
+for f in *.mp3; do
+    ((current_file++))
+    newname=$(namefix "$f")
+    printFormated "$f" "$newname" "$current_file" "$total_files"
+    rename "$f" "$newname"
 done
+
+printf "\033[${printedLines}A"  # Move cursor up
+printf "[==================================================] 100%\033[K\n"
+printf "[done]\033[K\n"
+printf "[total files renamed %d]\033[K\n" "$total_files"
+printf "\nRenaming completed.\033[K\n"
